021770c48773343bfacdae0c8c5c23940fb676dc
Edel <edelmedina322@gmail.com>
2022-08-09 12:18:59 -0300

    Only random placement fix and style left

--- src/Components/PlayingField.js
+++ src/Components/PlayingField.js
@@ -1,44 +1,60 @@
 import React, { useState, useEffect } from "react";
 const PlayingField = (props) => {
-    const [allCards, setAllCards] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-    const [clickedCards, setClickedCards] = useState([]);
-    const setDefault = () => {
-        setAllCards(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-        setClickedCards([]);
-    };
-    const makePlayingField = () => {
-        // Needs to be random, maybe randomize the grid area?  !!!!!!!!!!1
-        let playingField = document.getElementById("playing-field");
-        allCards.forEach( (element, index) => {
-            let card = document.createElement("button");
-            card.id = index;
-            card.addEventListener("click", function() {
-                checkCards(card.id);
-            });
-            playingField.append(card);
-        })          
-    };
-    const checkCards = (id) => {
-        if (id in clickedCards) lose();
-        else if (clickedCards.length === allCards.length) win();
-        else setClickedCards(current => [...current, id]);
-        console.log(clickedCards);
-    };
-    const win = () => {
-        console.log("win");
-    };
-    const lose = () => {
-        console.log("lose");
-    };
-
-    useEffect(() => {
-        makePlayingField();
+  const [status, setStatus] = useState(1);
+  let allCards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let clickedCards = [];
+  let usedSpots = [];
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const setDefault = () => {
+    allCards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    clickedCards = [];
+  };
+  const placeCard = () => {
+    let placement = getRandomInt(allCards.length);
+    if (usedSpots.includes(placement)) placeCard();
+    else {
+      usedSpots.push(placement);
+      console.log("going");
+      return placement;
+    }
+  };
+  const makePlayingField = () => {
+    // Needs to be random, maybe randomize the grid area?  !!!!!!!!!!
+    setDefault();
+    let playingField = document.getElementById("playing-field");
+    allCards.forEach((element, index) => {
+      let card = document.createElement("button");
+      card.classList.add(element, "card");
+      card.id = "card-" + index;
+      card.style.gridArea = placeCard();
+      console.log("happened");
+      console.log(index);
+      card.addEventListener("click", function () {
+        checkCards(index);
+      });
+      playingField.append(card);
     });
+  };
+  const checkCards = (index) => {
+    clickedCards.includes(index) ? lose() : clickedCards.push(index);
+    if (clickedCards.length === allCards.length) win();
+    console.log(clickedCards);
+  };
+  const win = () => {
+    console.log("win");
+  };
+  const lose = () => {
+    console.log("lose");
+  };
 
-    return (
-        <div id="playing-field">            
-        </div>
-    )
-}
+  useEffect(() => {
+    console.log("effect");
+    makePlayingField();
+  }, [status]);
+
+  return <div id="playing-field"></div>;
+};
 
 export default PlayingField;
